48ea5394a7c15f42ba8970679e8f2fcb13db7df3
“gaoxiang““ <gaoxiang@zhongwang.com>
2018-11-12 15:21:57 +0800

    QRcode

---
+++ src/renderer/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+import makeLogo from './makeLogo'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+Vue.prototype._makeLogo = makeLogo
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
